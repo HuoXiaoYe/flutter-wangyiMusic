8f4844be862f68249cdd4e6af6d0c4be3143d9f9
huoxiaoye <17711034@bjtu.edu.cn>
2019-08-04 23:55:10 +0800

    推荐歌单实现

--- lib/config/resource.dart
+++ lib/config/resource.dart
@@ -7,9 +7,62 @@ List wriperList = [
 ];
 
 List navList = [
-  {"desc":"每日推荐","img":"http://p.qpic.cn/music_cover/bglUEFSMMhA3eiblfHXvQRPLWicw02rzkpicmbqtaibV7MbEVruOa5g59A/300?n=1"},
-  {"desc":"歌单","img":"http://p.qpic.cn/music_cover/8icT3FJia3icDlR3nFE4vFIyd2tNho7tqTNyof0T1cmZJaibibIErUIVialQ/300?n=1"},
-  {"desc":"排行榜","img":"http://p.qpic.cn/music_cover/Uj77DagTFgiccudSicYvppRps3XU1ib4YFHdmTnkACW9L85ZXEkkkAUmA/300?n=1"},
-  {"desc":"电台","img":"http://p.qpic.cn/music_cover/4fNUrZe1KnJJVic1Aq97mkiavzuo8so2NbESRxkoVd4JfAMmUZP4HUDw/300?n=1"},
-  {"desc":"直播","img":"http://p.qpic.cn/music_cover/yaAoR5gcqYJQAXtyFibrhPRmqX1baoyIAvSPjaeJXPDKRD7zrbXtnpw/300?n=1"}
+  {
+    "desc": "每日推荐",
+    "img":
+        "http://p.qpic.cn/music_cover/bglUEFSMMhA3eiblfHXvQRPLWicw02rzkpicmbqtaibV7MbEVruOa5g59A/300?n=1"
+  },
+  {
+    "desc": "歌单",
+    "img":
+        "http://p.qpic.cn/music_cover/8icT3FJia3icDlR3nFE4vFIyd2tNho7tqTNyof0T1cmZJaibibIErUIVialQ/300?n=1"
+  },
+  {
+    "desc": "排行榜",
+    "img":
+        "http://p.qpic.cn/music_cover/Uj77DagTFgiccudSicYvppRps3XU1ib4YFHdmTnkACW9L85ZXEkkkAUmA/300?n=1"
+  },
+  {
+    "desc": "电台",
+    "img":
+        "http://p.qpic.cn/music_cover/4fNUrZe1KnJJVic1Aq97mkiavzuo8so2NbESRxkoVd4JfAMmUZP4HUDw/300?n=1"
+  },
+  {
+    "desc": "直播",
+    "img":
+        "http://p.qpic.cn/music_cover/yaAoR5gcqYJQAXtyFibrhPRmqX1baoyIAvSPjaeJXPDKRD7zrbXtnpw/300?n=1"
+  }
+];
+
+List recommendList = [
+  {
+    "desc": "正能量，拒绝低谷时的丧气值",
+    "img":
+        "http://p.qpic.cn/music_cover/bglUEFSMMhA3eiblfHXvQRPLWicw02rzkpicmbqtaibV7MbEVruOa5g59A/300?n=1"
+  },
+  {
+    "desc": "正能量，拒绝低谷时的丧气值",
+    "img":
+        "http://p.qpic.cn/music_cover/8icT3FJia3icDlR3nFE4vFIyd2tNho7tqTNyof0T1cmZJaibibIErUIVialQ/300?n=1"
+  },
+  {
+    "desc": "正能量，拒绝低谷时的丧气值",
+    "img":
+        "http://p.qpic.cn/music_cover/Uj77DagTFgiccudSicYvppRps3XU1ib4YFHdmTnkACW9L85ZXEkkkAUmA/300?n=1"
+  },
+  // {
+  //   "desc": "正能量，拒绝低谷时的丧气值",
+  //   "img":
+  //       "http://p.qpic.cn/music_cover/4fNUrZe1KnJJVic1Aq97mkiavzuo8so2NbESRxkoVd4JfAMmUZP4HUDw/300?n=1"
+  // },
+  // {
+  //   "desc": "正能量，拒绝低谷时的丧气值",
+  //   "img":
+  //       "http://p.qpic.cn/music_cover/yaAoR5gcqYJQAXtyFibrhPRmqX1baoyIAvSPjaeJXPDKRD7zrbXtnpw/300?n=1"
+  // },
+  // {
+  //   "desc": "正能量，拒绝低谷时的丧气值",
+  //   "img":
+  //       "http://p.qpic.cn/music_cover/bglUEFSMMhA3eiblfHXvQRPLWicw02rzkpicmbqtaibV7MbEVruOa5g59A/300?n=1"
+  // }
 ];
--- lib/pages/search/search.dart
+++ lib/pages/search/search.dart
@@ -12,8 +12,14 @@ class Search extends StatefulWidget {
 class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[Wriper(wriperList), NavBar(navList)],
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Wriper(wriperList),
+          NavBar(navList),
+          RecommendSongList(recommendList),
+        ],
+      ),
     );
   }
 }
@@ -62,17 +68,15 @@ class NavBar extends StatelessWidget {
               width: 60,
               height: 60,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(30)),
-                color: Colors.black12
-              ),
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                  color: Colors.black12),
               child: ClipOval(
                 child: Image.network(item["img"], width: 60),
               )),
           Text(item["desc"])
         ],
       ),
-      onTap: (){
-      },
+      onTap: () {},
     );
   }
 
@@ -87,6 +91,89 @@ class NavBar extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: navList.map((item) => _navItem(context, item)).toList(),
+      ),
+    );
+  }
+}
+
+// 推荐歌单
+
+class RecommendSongList extends StatelessWidget {
+  List recommendList;
+  RecommendSongList(this.recommendList) : super();
+
+  // 推荐歌单标题
+  Widget _title(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 15.0, right: 15.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            "推荐歌单",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w800),
+          ),
+          Container(
+            padding: EdgeInsets.only(bottom: 3, top: 3, left: 10, right: 10),
+            child: InkWell(
+              child: Text("歌单广场"),
+            ),
+            decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _recommendItem(BuildContext context, item) {
+    return InkWell(
+      child: Container(
+        width: 125,
+        height: 190,
+        color: Colors.blue,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Image.network(
+              item["img"],
+              width: 95,
+              // height: 100,
+              // fit: BoxFit.fill,
+            ),
+            Text(
+              item["desc"] + "akfdsujlkasjfkljasldf",
+              maxLines: 2,
+              // overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 400,
+      color: Colors.green,
+      child: Column(
+        children: <Widget>[
+          _title(context),
+          Container(
+            height: 300,
+            width: 600,
+            color: Colors.red,
+            child: GridView.count(
+              scrollDirection: Axis.vertical,
+              crossAxisCount: 3,
+              children: recommendList
+                  .map((item) => _recommendItem(context, item))
+                  .toList()
+            ),
+          )
+        ],
       ),
     );
   }
