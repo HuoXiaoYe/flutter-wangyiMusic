8a3782cb0e38d15a8aacc999d7e4dff6f7d83f2e
huoxiaoye <17711034@bjtu.edu.cn>
2019-08-04 21:20:12 +0800

    头部导航栏模仿完毕

---
+++ lib/pages/home_content.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class HomeContent extends StatefulWidget {
+  @override
+  _HomeContentState createState() => _HomeContentState();
+}
+
+class _HomeContentState extends State<HomeContent>
+    with SingleTickerProviderStateMixin {
+  TabController _controller;
+
+  @override
+  void initState() {
+    _controller = new TabController(initialIndex: 1,length: 4, vsync: this);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          // 顶部导航栏
+          Container(
+            height: 20,
+          ),
+          Container(
+            height: 40.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 8.0),
+                  child: InkWell(
+                    child: Icon(
+                      Icons.list,
+                      size: 28,
+                    ),
+                    onTap: () {},
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  width: 240,
+                  padding: EdgeInsets.only(top: 10.0),
+                  // color: Colors.white,
+                  child: TabBar(
+                    controller: _controller,
+                    indicatorSize: TabBarIndicatorSize.label,
+                    // labelPadding: EdgeInsets.only(bottom: -2.0),
+                    // unselectedLabelColor: Colors.red,
+                    labelStyle: TextStyle(color: Colors.pink),
+                    tabs: <Widget>[
+                      Text(
+                        "我的",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      Text(
+                        "发现",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      Text(
+                        "云村",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      Text(
+                        "视频",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 8.0),
+                  child: InkWell(
+                    child: Icon(
+                      Icons.search,
+                      size: 28,
+                    ),
+                    onTap: () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+        
+          // 主体区域
+          TabBarView(
+            controller: _controller,
+            children: <Widget>[
+              
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
