bcd72bddf3fbb40f51809d20e14d623ad02ce5c3
huoxiaoye <17711034@bjtu.edu.cn>
2019-08-15 19:59:52 +0800

    歌曲播放页面

--- lib/pages/play/play.dart
+++ lib/pages/play/play.dart
@@ -4,20 +4,44 @@ class SongPlay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          primarySwatch: Colors.pink
-          ),
+      theme: ThemeData(primarySwatch: Colors.pink),
       home: Scaffold(
         appBar: AppBar(
           title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              InkWell(
-                onTap: (){
-                  Navigator.of(context).pop();
-                },
-                child: Icon(Icons.navigate_before),
-              ),
-              Text("一眼万年")
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Icon(
+                        Icons.navigate_before,
+                        size: 38,
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "走在冷风中",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        Text(
+                          "周二珂",
+                          style: TextStyle(fontSize: 12),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+              ,Container(
+                child: Icon(Icons.share),
+              )
             ],
           ),
         ),
